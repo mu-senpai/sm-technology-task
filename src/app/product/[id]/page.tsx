46c30f084fea9767fc08054e777bf3701106e07e
Abdulla Al Muhit <muhitabdullah279@gmail.com>
2025-05-08 05:52:14 +0530

    Product details page has updated again.

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { Chip, CircularProgress } from "@mui/material";
 import { useGetProductByIdQuery, useGetProductsQuery } from "@/lib/features/apiSlice";
 import { Product } from "@/app/(home)/components/ProductCard";
